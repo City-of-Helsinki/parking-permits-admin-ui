6b7cbef76339a6c70fc8e349d431091d6cfef008
Mingfeng Yu <mfdevworld@gmail.com>
2021-09-21 10:08:29 +0300

    Create dummy pages
    Refs: PV-223

---
+++ src/pages/Messages.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Messages = (): React.ReactElement => <div>This is messages page</div>;
+export default Messages;
---
+++ src/pages/Returns.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Returns = (): React.ReactElement => <div>This is returns page</div>;
+export default Returns;
