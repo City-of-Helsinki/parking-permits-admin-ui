9fe23b0e3c3008efa020f6e5779ccc736beffc83
Mingfeng Yu <mfdevworld@gmail.com>
2021-09-27 18:25:57 +0300

    Fix app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (str: string) => str,
+    i18n: {
+      language: 'fi',
+      changeLanguage: () => new Promise(() => {}),
+    },
+  }),
+}));
+
+const unAuthenticatedClient = {
+  getUser: () => null,
+  isInitialized: () => false,
+  isAuthenticated: () => false,
+};
+
+jest.mock('./auth/hooks', () => ({
+  useClient: () => unAuthenticatedClient,
+}));
+
+test('renders app title', () => {
+  render(<App />, { wrapper: MemoryRouter });
+  const titles = screen.getAllByText(/components.header.appTitle/i);
+  expect(titles[0]).toBeInTheDocument();
 });
