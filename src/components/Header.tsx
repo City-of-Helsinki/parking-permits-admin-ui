3a199d4f676dc29431a33e6ea172e16fc25627e2
Mingfeng Yu <mfdevworld@gmail.com>
2021-09-27 14:15:32 +0300

    Populate Navigation.User with authenticated user info
    Refs: PV-213

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { Navigation } from 'hds-react';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { matchPath, useLocation } from 'react-router-dom';
+import { useClient } from '../auth/hooks';
 
 const T_PATH = 'components.header';
 
@@ -9,6 +10,9 @@ const isActiveLink = (path: string, currentPath: string): boolean =>
   !!matchPath({ path, end: false }, currentPath);
 
 const Header = (): React.ReactElement => {
+  const client = useClient();
+  const user = client.getUser();
+  const userName = user ? `${user.given_name} ${user.family_name}` : '';
   const { t, i18n } = useTranslation();
   const title = t(`${T_PATH}.appTitle`);
   const location = useLocation();
@@ -37,25 +41,37 @@ const Header = (): React.ReactElement => {
       logoLanguage={i18n.language === 'sv' ? 'sv' : 'fi'}
       menuToggleAriaLabel="menu"
       skipTo="#content"
-      skipToContentLabel="Skip to content">
+      skipToContentLabel={`${T_PATH}.skipToContent`}>
       <Navigation.Actions>
         <Navigation.LanguageSelector label="FI">
           <Navigation.Item lang="fi" label="Suomeksi" />
           <Navigation.Item lang="sv" label="På svenska" />
           <Navigation.Item lang="en" label="In English" />
         </Navigation.LanguageSelector>
-        <Navigation.User label="Sign in" />
-      </Navigation.Actions>
-      <Navigation.Row>
-        {navLinks.map(({ path, label }) => (
+        <Navigation.User
+          authenticated={client.isAuthenticated()}
+          label={`${T_PATH}.login`}
+          onSignIn={(): void => client.login()}
+          userName={userName}>
           <Navigation.Item
-            key={path}
-            href={path}
-            label={label}
-            active={isActiveLink(path, location.pathname)}
+            onClick={(): void => client.logout()}
+            variant="secondary"
+            label={`${T_PATH}.logout`}
           />
-        ))}
-      </Navigation.Row>
+        </Navigation.User>
+      </Navigation.Actions>
+      {client.isAuthenticated() && (
+        <Navigation.Row>
+          {navLinks.map(({ path, label }) => (
+            <Navigation.Item
+              key={path}
+              href={path}
+              label={label}
+              active={isActiveLink(path, location.pathname)}
+            />
+          ))}
+        </Navigation.Row>
+      )}
     </Navigation>
   );
 };
