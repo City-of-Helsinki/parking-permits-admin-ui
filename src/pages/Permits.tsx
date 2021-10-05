904a79cc47cfcd2903fd3080b7712eaaf020a68c
Mingfeng Yu <mfdevworld@gmail.com>
2021-10-05 15:33:09 +0300

    Load API tokens when accessing private pages
    Refs: PV-217

--- src/pages/Permits.tsx
+++ src/pages/Permits.tsx
@@ -1,5 +1,35 @@
+import { gql, useQuery } from '@apollo/client';
 import React from 'react';
 import { makePrivate } from './utils';
 
-const Permits = (): React.ReactElement => <div>This is permits page</div>;
+const PERMITS_QUERY = gql`
+  query GetPermits($pageInput: PageInput!) {
+    permits(pageInput: $pageInput) {
+      objects {
+        customer {
+          firstName
+          lastName
+        }
+      }
+      pageInfo {
+        numPages
+        next
+        prev
+      }
+    }
+  }
+`;
+
+const Permits = (): React.ReactElement => {
+  const { loading, error, data } = useQuery(PERMITS_QUERY, {
+    variables: { pageInput: { page: 1 } },
+  });
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>{JSON.stringify(error)}</div>;
+  }
+  return <div>{JSON.stringify(data)}</div>;
+};
 export default makePrivate(Permits);
--- src/pages/utils.tsx
+++ src/pages/utils.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Navigate } from 'react-router';
-import { useClient } from '../auth/hooks';
+import { ApiAccessTokenContext } from '../auth/ApiAccessTokenProvider';
+import { ApiAccessTokenActions, useClient } from '../auth/hooks';
 
 // eslint-disable-next-line import/prefer-default-export
 export function makePrivate<T>(
@@ -8,11 +9,18 @@ export function makePrivate<T>(
 ): React.ComponentType<T> {
   return function PrivateComponent(props: T) {
     const client = useClient();
+    const { getStatus } = useContext(
+      ApiAccessTokenContext
+    ) as ApiAccessTokenActions;
+    const apiStatus = getStatus();
     if (!client.isInitialized()) {
       return <div>Initializing...</div>;
     }
     if (!client.isAuthenticated()) {
       return <Navigate to="/login" />;
+    }
+    if (apiStatus !== 'loaded') {
+      return <div>Loading...</div>;
     }
     return <Component {...props} />;
   };
