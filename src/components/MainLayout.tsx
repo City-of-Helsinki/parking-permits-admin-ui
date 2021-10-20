d054ad61f069bb08bca5355263f5780963ac95ce
Mingfeng Yu <mfdevworld@gmail.com>
2021-10-20 17:29:16 +0300

    Add a divider line after navigation links
    Refs: PV-254

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Divider from './common/Divider';
 import Header from './Header';
 import styles from './MainLayout.module.scss';
 
 const MainLayout = (): React.ReactElement => (
   <div className={styles['main-layout']}>
     <Header />
+    <Divider />
     <div className={styles['content-container']}>
       <Outlet />
     </div>
