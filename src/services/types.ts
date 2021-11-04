932d5442fc79f52e8eff46679bee43a9f93058b3
Mingfeng Yu <mfdevworld@gmail.com>
2021-11-04 13:48:50 +0200

    Add CreateResidentPermis PersonalInfo component
    Refs: PV-228

---
+++ src/services/types.ts
@@ -0,0 +1,32 @@
+import { ParkingZone } from '../types';
+
+export interface TrafiComUser {
+  firstName: string;
+  lastName: string;
+  nationalIdNumber: string;
+}
+
+export interface TrafiComVehicle {
+  isLowEmission: boolean;
+  consentLowEmissionDiscount: boolean;
+  registrationNumber: string;
+  manufacturer: string;
+  model: string;
+  type: string;
+  serialNumber: string;
+  holder: TrafiComUser;
+  owner: TrafiComUser;
+  otherHolder?: TrafiComUser;
+}
+
+export interface Person {
+  firstName: string;
+  lastName: string;
+  address: string;
+  addressSecurityBan: string;
+  nationalIdNumber: string;
+  zone: ParkingZone;
+  phoneNumber: string;
+  email: string;
+  driverLicenseChecked: boolean;
+}
