a989e89650c69d62b0b849a70f310f5f52164329
Mingfeng Yu <mfdevworld@gmail.com>
2021-11-19 16:15:21 +0200

    Add end permit dialog
    Refs: PV-230

---
+++ src/components/permitDetail/EndPermitDialog.tsx
@@ -0,0 +1,68 @@
+import { Button, Dialog, RadioButton } from 'hds-react';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { PermitEndType } from '../../types';
+import { formatDateTimeDisplay } from '../../utils';
+import styles from './EndPermitDialog.module.scss';
+
+const T_PATH = 'components.permitDetail.endPermitDialog';
+
+interface EndPermitDialogProps {
+  isOpen: boolean;
+  currentPeriodEndTime: string;
+  onCancel: () => void;
+  onConfirm: (endType: PermitEndType) => void;
+}
+
+const EndPermitDialog = ({
+  isOpen,
+  currentPeriodEndTime,
+  onCancel,
+  onConfirm,
+}: EndPermitDialogProps): React.ReactElement => {
+  const { t } = useTranslation();
+  const [endType, setEndType] = useState<PermitEndType | undefined>();
+  return (
+    <Dialog
+      id="confirm-end-permit"
+      aria-labelledby="confirmEndPermit"
+      isOpen={isOpen}>
+      <Dialog.Header id="confirmEndPermit" title={t(`${T_PATH}.title`)} />
+      <Dialog.Content>
+        <div className={styles.chooseEndTypeLabel}>
+          {t(`${T_PATH}.choosePermitEndType`)}
+        </div>
+        <RadioButton
+          id="endImmediately"
+          name="endType"
+          label={t(`${T_PATH}.endImmediately`)}
+          value={PermitEndType.IMMEDIATELY}
+          checked={endType === PermitEndType.IMMEDIATELY}
+          onChange={e => setEndType(e.target.value as PermitEndType)}
+        />
+        <RadioButton
+          id="endAfterCurrentPeriod"
+          name="endType"
+          label={t(`${T_PATH}.endAfterCurrentPeriod`)}
+          value={PermitEndType.AFTER_CURRENT_PERIOD}
+          checked={endType === PermitEndType.AFTER_CURRENT_PERIOD}
+          onChange={e => setEndType(e.target.value as PermitEndType)}
+        />
+        <div className={styles.currentPeriodEndTime}>
+          {formatDateTimeDisplay(currentPeriodEndTime)}
+        </div>
+      </Dialog.Content>
+      <Dialog.ActionButtons>
+        <Button
+          disabled={!endType}
+          onClick={() => endType && onConfirm(endType)}>
+          {t(`${T_PATH}.continue`)}
+        </Button>
+        <Button variant="secondary" onClick={() => onCancel()}>
+          {t(`${T_PATH}.cancel`)}
+        </Button>
+      </Dialog.ActionButtons>
+    </Dialog>
+  );
+};
+export default EndPermitDialog;
