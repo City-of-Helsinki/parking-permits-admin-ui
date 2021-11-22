1bdb04503acd92cd5f9f3b0e21de97c0a2cd1f76
Mingfeng Yu <mfdevworld@gmail.com>
2021-11-22 17:44:38 +0200

    Add end permit page and refund components
    Refs: PV-230

---
+++ src/components/permitDetail/RefundInfoFixedPeriod.tsx
@@ -0,0 +1,59 @@
+import { TextInput } from 'hds-react';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { PermitDetail } from '../../types';
+import { formatMonthlyPrice } from '../../utils';
+import styles from './RefundInfoFixedPeriod.module.scss';
+
+const T_PATH = 'components.permitDetail.refundInfoFixedPeriod';
+export interface RefundInfoFixedPeriodProps {
+  className?: string;
+  iban: string;
+  permit: PermitDetail;
+  onChangeIban: (iban: string) => void;
+}
+
+const RefundInfoFixedPeriod = ({
+  className,
+  iban,
+  permit,
+  onChangeIban,
+}: RefundInfoFixedPeriodProps): React.ReactElement => {
+  const { t } = useTranslation();
+  const { monthsLeft, monthlyPrice } = permit;
+  const refundAmount = `${(-monthsLeft * monthlyPrice).toFixed(2)} €`;
+  const amountLabel = t(`${T_PATH}.monthCount`, {
+    count: monthsLeft,
+  });
+  const refundDetail = `${amountLabel}, ${formatMonthlyPrice(monthlyPrice)}`;
+  return (
+    <div className={className}>
+      <div className={styles.title}>{t(`${T_PATH}.title`)}</div>
+      <div className={styles.infoBox}>
+        <div className={styles.orderRemaining}>
+          <div className={styles.orderRemainingLabel}>
+            {t(`${T_PATH}.orderRemaining`)}
+          </div>
+          <div className={styles.orderRemainingAmount}>{refundAmount}</div>
+        </div>
+        <div className={styles.orderRemainingDetail}>{refundDetail}</div>
+        <div className={styles.refundDescription}>
+          * {t(`${T_PATH}.orderRemaingDescription`)}
+        </div>
+        <div className={styles.refund}>
+          <div className={styles.refundLabel}>{t(`${T_PATH}.refund`)}</div>
+          <div className={styles.refundAmount}>{refundAmount}</div>
+        </div>
+        <TextInput
+          className={styles.iban}
+          required
+          id="iban"
+          label="IBAN"
+          value={iban}
+          onChange={e => onChangeIban(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+export default RefundInfoFixedPeriod;
