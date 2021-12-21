37e626b392bcfa4fd1de8b9ffc18716a4bc4c00c
Mingfeng Yu <mfdevworld@gmail.com>
2021-12-21 14:31:10 +0200

    Show multiple prices for vehicle info in permit detail page
    Refs: PV-293

--- src/components/permitDetail/VehicleInfo.tsx
+++ src/components/permitDetail/VehicleInfo.tsx
@@ -1,8 +1,9 @@
 import { Checkbox } from 'hds-react';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { PermitDetail } from '../../types';
-import { formatMonthlyPrice, formatVehicleName } from '../../utils';
+import { PermitDetail, PriceModifiers } from '../../types';
+import { formatVehicleName } from '../../utils';
+import ProductPriceRow from '../common/ProductPriceRow';
 import styles from './VehicleInfo.module.scss';
 
 const T_PATH = 'components.permitDetail.vehicleInfo';
@@ -19,7 +20,10 @@ const VehicleInfo = ({
   const { t } = useTranslation();
   const { vehicle, parkingZone, consentLowEmissionAccepted } = permit;
   const { isLowEmission } = vehicle;
-  const { residentPrice } = parkingZone;
+  const priceModifiers: PriceModifiers = {
+    isLowEmission,
+    isSecondaryVehicle: false,
+  };
   return (
     <div className={className}>
       <div className={styles.title}>{t(`${T_PATH}.title`)}</div>
@@ -32,18 +36,15 @@ const VehicleInfo = ({
             }`
           )}
         </div>
-        <div className={styles.priceInfo}>
-          {isLowEmission && (
-            <span>
-              <strong>{formatMonthlyPrice(residentPrice / 2)}</strong>
-            </span>
-          )}
-          <span
-            style={{
-              textDecoration: isLowEmission ? 'line-trhough' : 'none',
-            }}>
-            {formatMonthlyPrice(residentPrice)}
-          </span>
+        <div className={styles.priceList}>
+          {parkingZone.residentProducts?.map(product => (
+            <ProductPriceRow
+              key={product.startDate}
+              className={styles.productPriceRow}
+              product={product}
+              priceModifiers={priceModifiers}
+            />
+          ))}
         </div>
         <Checkbox
           disabled
