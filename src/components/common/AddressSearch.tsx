c57bfffb78a72608a2ecffef161c3d794054a3b5
Mingfeng Yu <mfdevworld@gmail.com>
2022-01-18 15:47:14 +0200

    Add an AddressSearch component
    HDS does not have a ready made component for remote search
    that displays a dropdown list for search results. The closest
    ones are SearchInput and Combobox, but both of them are lacking
    features for remote search.
    
    SearchInput does not provide a way to return the selected
    suggestion object (it only returns the label). While Combobox
    does not provide a way to react to the typing changes.
    
    The AddressSearch component is adapted from SearchInput, we
    remembers the address search results to the components and
    then find the right address from the selected label.
    
    Refs: PV-311

---
+++ src/components/common/AddressSearch.tsx
@@ -0,0 +1,98 @@
+import {
+  Button,
+  IconArrowLeft,
+  IconPen,
+  SearchInput,
+  TextInput,
+} from 'hds-react';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import addressSearch from '../../services/addressSearch';
+import { Address, AddressInput } from '../../types';
+import { formatAddress } from '../../utils';
+
+const T_PATH = 'components.common.addressSearch';
+
+interface AddressSearchProps {
+  className?: string;
+  label: string;
+  address?: Address;
+  onSelect: (address: AddressInput) => void;
+}
+
+interface AddressSuggestionItem {
+  label: string;
+  address: AddressInput;
+}
+
+const AddressSearch = ({
+  className,
+  label,
+  address,
+  onSelect,
+}: AddressSearchProps): React.ReactElement => {
+  const { t, i18n } = useTranslation();
+  const [isEditing, setIsEditing] = useState(false);
+  let searchSuggestions: AddressSuggestionItem[] = [];
+  const getSuggestions = (inputValue: string) =>
+    addressSearch.search(inputValue).then(addresses => {
+      searchSuggestions = addresses.map(_address => ({
+        label: formatAddress(_address, 'fi'),
+        address: _address,
+      }));
+      return searchSuggestions;
+    });
+
+  const handleSubmit = (value: string) => {
+    const item = searchSuggestions.find(
+      suggestion => suggestion.label === value
+    );
+    if (item) {
+      onSelect(item.address);
+      setIsEditing(false);
+    }
+  };
+
+  if (isEditing) {
+    return (
+      <>
+        <SearchInput
+          hideSearchButton
+          className={className}
+          label={label}
+          clearButtonAriaLabel={t(`${T_PATH}.clear`)}
+          suggestionLabelField="label"
+          getSuggestions={getSuggestions}
+          onSubmit={handleSubmit}
+        />
+        <Button
+          variant="supplementary"
+          size="small"
+          iconLeft={<IconArrowLeft />}
+          onClick={() => setIsEditing(false)}>
+          {t(`${T_PATH}.cancel`)}
+        </Button>
+      </>
+    );
+  }
+  return (
+    <>
+      <TextInput
+        readOnly
+        className={className}
+        id="address"
+        label={label}
+        value={address ? formatAddress(address, i18n.language) : '-'}
+      />
+      <Button
+        variant="supplementary"
+        size="small"
+        iconLeft={<IconPen />}
+        onClick={() => setIsEditing(true)}>
+        {t(`${T_PATH}.edit`)}
+      </Button>
+    </>
+  );
+};
+
+export default AddressSearch;
