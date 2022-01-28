0f4b213708cb6553a5e5ddeeaffefe6ac7c1bed8
Mingfeng Yu <mfdevworld@gmail.com>
2022-01-28 22:22:24 +0200

    Rename Returns page to Refund
    Refs: MHR-312

--- src/pages/Returns.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { makePrivate } from '../auth/utils';
-
-const Returns = (): React.ReactElement => <div>This is returns page</div>;
-export default makePrivate(Returns);
