e0949645c7a3efe14981f387981db72c143fe5db
Mingfeng Yu <mfdevworld@gmail.com>
2022-02-17 11:03:00 +0200

    Add a general purpose confirm dialog component

---
+++ src/components/common/ConfirmDialog.tsx
@@ -0,0 +1,47 @@
+import { Button, Dialog } from 'hds-react';
+import React from 'react';
+
+interface ConfirmDialogProps {
+  isOpen: boolean;
+  title: string;
+  message: string;
+  secondaryMessage?: string;
+  confirmLabel: string;
+  cancelLabel: string;
+  onCancel: () => void;
+  onConfirm: () => void;
+}
+
+const DIALOG_ID = 'confirm-dialog';
+const DIALOG_TITLE_ID = 'confirm-dialog-title';
+const DIALOG_DESCRIPTION_ID = 'confirm-dialog-description';
+
+const ConfirmDialog = ({
+  isOpen,
+  title,
+  message,
+  secondaryMessage,
+  confirmLabel,
+  cancelLabel,
+  onConfirm,
+  onCancel,
+}: ConfirmDialogProps): React.ReactElement => (
+  <Dialog
+    id={DIALOG_ID}
+    aria-labelledby={DIALOG_TITLE_ID}
+    aria-describedby={DIALOG_DESCRIPTION_ID}
+    isOpen={isOpen}>
+    <Dialog.Header id={DIALOG_TITLE_ID} title={title} />
+    <Dialog.Content>
+      <p id={DIALOG_DESCRIPTION_ID}>{message}</p>
+      {secondaryMessage && <p>{secondaryMessage}</p>}
+    </Dialog.Content>
+    <Dialog.ActionButtons>
+      <Button onClick={() => onConfirm()}>{confirmLabel}</Button>
+      <Button variant="secondary" onClick={() => onCancel()}>
+        {cancelLabel}
+      </Button>
+    </Dialog.ActionButtons>
+  </Dialog>
+);
+export default ConfirmDialog;
