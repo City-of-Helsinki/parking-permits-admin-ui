4979223e8b972a31151a7dcf679d3db092493401
Mingfeng Yu <mfdevworld@gmail.com>
2022-04-12 17:41:26 +0300

    Add a EuroClassSelect component
    Refs: PV-389

---
+++ src/components/common/EuroClassSelect.tsx
@@ -0,0 +1,58 @@
+import { Select } from 'hds-react';
+import React from 'react';
+
+interface EuroClassOption {
+  label: string;
+  value: number;
+}
+
+interface EuroClassSelectProps {
+  className?: string;
+  label: string;
+  value: number;
+  onChange: (value: number) => void;
+}
+
+const EURO_CLASS_OPTIONS: EuroClassOption[] = [
+  {
+    label: 'Euro 1',
+    value: 1,
+  },
+  {
+    label: 'Euro 2',
+    value: 2,
+  },
+  {
+    label: 'Euro 3',
+    value: 3,
+  },
+  {
+    label: 'Euro 4',
+    value: 3,
+  },
+  {
+    label: 'Euro 5',
+    value: 5,
+  },
+  {
+    label: 'Euro 6',
+    value: 6,
+  },
+];
+
+const EuroClassSelect = ({
+  label,
+  value,
+  className,
+  onChange,
+}: EuroClassSelectProps): React.ReactElement => (
+  <Select
+    className={className}
+    label={label}
+    options={EURO_CLASS_OPTIONS}
+    value={{ label: `Euro ${value}`, value }}
+    onChange={(option: EuroClassOption) => onChange(option.value)}
+  />
+);
+
+export default EuroClassSelect;
