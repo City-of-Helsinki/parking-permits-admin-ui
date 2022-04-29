c04be67e800e8e578941e56e99ddebc76cc38016
Mingfeng Yu <mfdevworld@gmail.com>
2022-04-29 16:53:42 +0300

    Add a EditAddress page
    Refs: PV-357

---
+++ src/pages/superAdmin/addresses/EditAddress.tsx
@@ -0,0 +1,104 @@
+import { gql, useMutation, useQuery } from '@apollo/client';
+import { Notification } from 'hds-react';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate, useParams } from 'react-router';
+import { makePrivate } from '../../../auth/utils';
+import AddressForm from '../../../components/superAdmin/addresses/AddressForm';
+import { Address, MutationResponse } from '../../../types';
+import styles from './EditAddress.module.scss';
+
+const T_PATH = 'pages.superAdmin.addresses.editAddress';
+
+const ADDRESS_QUERY = gql`
+  query GetAddress($addressId: ID!) {
+    address(addressId: $addressId) {
+      id
+      streetName
+      streetNameSv
+      streetNumber
+      postalCode
+      city
+      citySv
+      location
+      zone {
+        name
+      }
+    }
+  }
+`;
+
+const UPDATE_ADDRESS_MUTATION = gql`
+  mutation UpdateAddress($addressId: ID!, $address: AddressInput!) {
+    updateAddress(addressId: $addressId, address: $address) {
+      success
+    }
+  }
+`;
+
+const DELETE_ADDRESS_MUTATION = gql`
+  mutation DeleteAddress($addressId: ID!) {
+    deleteAddress(addressId: $addressId) {
+      success
+    }
+  }
+`;
+
+const EditAddress = (): React.ReactElement => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState('');
+  const { id: addressId } = useParams();
+  const variables = { addressId };
+  const { loading, data } = useQuery<{ address: Address }>(ADDRESS_QUERY, {
+    variables,
+    onError: error => setErrorMessage(error.message),
+  });
+  const [updateAddress] = useMutation<MutationResponse>(
+    UPDATE_ADDRESS_MUTATION,
+    {
+      onCompleted: () => navigate('/admin/addresses'),
+      onError: e => setErrorMessage(e.message),
+    }
+  );
+  const [deleteAddress] = useMutation<MutationResponse>(
+    DELETE_ADDRESS_MUTATION,
+    {
+      onCompleted: () => navigate('/admin/addresses'),
+      onError: e => setErrorMessage(e.message),
+    }
+  );
+  if (loading) {
+    return <div>loading...</div>;
+  }
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>{t(`${T_PATH}.title`)}</h2>
+      <div className={styles.content}>
+        {data?.address && (
+          <AddressForm
+            onSubmit={address =>
+              updateAddress({ variables: { addressId, address } })
+            }
+            onDelete={() => deleteAddress({ variables: { addressId } })}
+            address={data.address}
+            className={styles.addressForm}
+          />
+        )}
+      </div>
+      {errorMessage && (
+        <Notification
+          type="error"
+          label={t('message.error')}
+          position="bottom-center"
+          dismissible
+          closeButtonLabelText={t('message.close')}
+          onClose={() => setErrorMessage('')}
+          style={{ zIndex: 100 }}>
+          {errorMessage}
+        </Notification>
+      )}
+    </div>
+  );
+};
+export default makePrivate(EditAddress);
--- src/routes.tsx
+++ src/routes.tsx
@@ -18,6 +18,7 @@ import RefundDetail from './pages/RefundDetail';
 import Refunds from './pages/Refunds';
 import Reports from './pages/Reports';
 import Addresses from './pages/superAdmin/addresses/Addresses';
+import EditAddress from './pages/superAdmin/addresses/EditAddress';
 import CreateProduct from './pages/superAdmin/products/CreateProduct';
 import EditProduct from './pages/superAdmin/products/EditProduct';
 import Products from './pages/superAdmin/products/Products';
@@ -31,6 +32,7 @@ const routes = [
       { path: 'products/create', element: <CreateProduct /> },
       { path: 'products/:id', element: <EditProduct /> },
       { path: 'addresses', element: <Addresses /> },
+      { path: 'addresses/:id', element: <EditAddress /> },
       { path: '', element: <Navigate to="products" /> },
     ],
   },
