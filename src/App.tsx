5ee89fcedd3d406450ff3fc3a58196d2550f831c
Aman Yadav <amanpdyadav@gmail.com>
2022-09-18 12:44:04 -0700

    Revert "Add Cookiehub widget"
    This reverts commit 5ba3142883325af8e62a03d823eb6c7671048b5c.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import { useRoutes } from 'react-router-dom';
 import routes from './routes';
 
 function App(): React.ReactElement {
   const routing = useRoutes(routes);
-
-  return (
-    <>
-      <Helmet>
-        <script type="text/javascript">
-          {`
-            var cpm = {
-              enabled: ${process.env.REACT_APP_COOKIEHUB_ENABLED}
-            };
-            (function(h,u,b){
-            var d=h.getElementsByTagName("script")[0],e=h.createElement("script");
-            e.async=true;e.src='${process.env.REACT_APP_COOKIEHUB_URL}';
-            e.onload=function(){u.cookiehub.load(b);}
-            d.parentNode.insertBefore(e,d);
-            })(document,window,cpm);
-        `}
-        </script>
-      </Helmet>
-      {routing}
-    </>
-  );
+  return <>{routing}</>;
 }
 
 export default App;
