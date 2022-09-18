afe7c7a6f0dc2121068ed9375f96df20ab3f9f01
Aman Yadav <amanpdyadav@gmail.com>
2022-09-18 23:30:57 +0300

    Fix tests
    Refs PV-257

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+// eslint-disable-next-line import/no-named-default,import/no-extraneous-dependencies
+import { default as RO } from 'resize-observer-polyfill';
+
+global.ResizeObserver = RO;
