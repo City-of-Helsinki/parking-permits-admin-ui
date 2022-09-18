a438f2ea07f8aaec3883f2c56664004fef2a8b1c
Aman Yadav <amanpdyadav@gmail.com>
2022-09-18 22:57:52 +0300

    Add hds cookie consent
    Refs PV-257

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
+import { CookieModal } from 'hds-react/components';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { useRoutes } from 'react-router-dom';
 import routes from './routes';
 
 function App(): React.ReactElement {
+  const { t, i18n } = useTranslation();
   const routing = useRoutes(routes);
-  return <>{routing}</>;
+  const contentSource = {
+    siteName: t('components.header.appTitle'),
+    title: t('components.header.appTitle'),
+    currentLanguage: i18n.language as 'fi' | 'sv' | 'en',
+    optionalCookies: {
+      cookies: [
+        {
+          commonGroup: 'statistics',
+          commonCookie: 'matomo',
+        },
+      ],
+    },
+    language: {
+      onLanguageChange: (code: string) => i18n.changeLanguage(code),
+    },
+    focusTargetSelector: '#parking-permit-admin-app',
+    // TODO: Find some way not to break the load without reloading.
+    onAllConsentsGiven: () => window.location.reload(),
+  };
+  return (
+    <>
+      <div id="parking-permit-admin-app">{routing}</div>
+      <CookieModal contentSource={contentSource} />
+    </>
+  );
 }
 
 export default App;
