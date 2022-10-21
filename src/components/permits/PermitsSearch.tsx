51befc0786e22b0cd769e49050fbe34edf439715
Daniel Prange <prange.daniel@gmail.com>
2022-10-21 12:52:18 +0300

    Add search button in permits

--- src/components/permits/PermitsSearch.tsx
+++ src/components/permits/PermitsSearch.tsx
@@ -1,4 +1,4 @@
-import { SearchInput } from 'hds-react';
+import { Button, IconSearch, SearchInput } from 'hds-react';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PermitSearchParams } from '../../types';
@@ -8,7 +8,7 @@ import StatusSelect from './StatusSelect';
 const T_PATH = 'components.permits.permitsSearch';
 export interface PermitsSearchProps {
   searchParams: PermitSearchParams;
-  onSearch: (searchInfo: PermitSearchParams) => void;
+  onSearch: (newSearchParams: PermitSearchParams) => void;
 }
 
 const PermitsSearch = ({
@@ -16,22 +16,34 @@ const PermitsSearch = ({
   onSearch,
 }: PermitsSearchProps): React.ReactElement => {
   const { t } = useTranslation();
-  const [localSearchParams, setLocalSearchParams] = useState(searchParams);
-  const { status, q } = localSearchParams;
+
+  const [status, setStatus] = useState(searchParams.status);
+  const [query, setQuery] = useState(searchParams.q);
+
+  const handleSubmit = () => onSearch({ status, q: query });
+
   return (
     <div className={styles.container}>
       <div className={styles.row}>
         <StatusSelect
           className={styles.statusSelect}
           value={status}
-          onChange={newStatus => setLocalSearchParams({ q, status: newStatus })}
+          onChange={setStatus}
         />
         <SearchInput
           className={styles.SearchInput}
           label=""
-          placeholder={q || t(`${T_PATH}.placeholder`)}
-          onSubmit={value => onSearch({ ...localSearchParams, q: value })}
+          placeholder={searchParams.q || t(`${T_PATH}.placeholder`)}
+          onSubmit={handleSubmit}
+          onChange={setQuery}
+          hideSearchButton
         />
+        <Button
+          className={styles.searchButton}
+          iconLeft={<IconSearch />}
+          onClick={handleSubmit}>
+          {t(`${T_PATH}.searchButton`)}
+        </Button>
       </div>
     </div>
   );
