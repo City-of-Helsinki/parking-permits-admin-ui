51ae2f5b03b4ece12736adcdd718b51f69bebaae
Daniel Prange <prange.daniel@gmail.com>
2022-10-21 14:53:53 +0300

    Add search button in refunds

--- src/components/refunds/RefundsSearch.tsx
+++ src/components/refunds/RefundsSearch.tsx
@@ -1,9 +1,10 @@
-import { formatISO } from 'date-fns';
-import { Checkbox, DateInput, SearchInput, SelectionGroup } from 'hds-react';
+import { formatISO, parse } from 'date-fns';
+import { Button, DateInput, IconSearch, SearchInput } from 'hds-react';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PaymentType, RefundSearchParams } from '../../types';
-import { formatDateDisplay } from '../../utils';
+import { joinSet } from '../../utils';
+import FilterOptions from '../orders/FilterOptions';
 import styles from './RefundsSearch.module.scss';
 import RefundStatusSelect from './RefundStatusSelect';
 
@@ -19,44 +20,50 @@ const RefundsSearch = ({
   onSearch,
 }: RefundsSearchProps): React.ReactElement => {
   const { t } = useTranslation();
-  // the search only trigged when user clicks the search button
-  // so we need to keep a local state of search params
-  const [localSearchParams, setLocalSearchParams] = useState(searchParams);
-  const { q, startDate, endDate, status, paymentTypes } = localSearchParams;
-  const paymentTypeArray = paymentTypes.split(',');
+
+  const [status, setStatus] = useState(searchParams.status);
+  const [query, setQuery] = useState(searchParams.q);
+  const [startDate, setStartDate] = useState(searchParams.startDate);
+  const [endDate, setEndDate] = useState(searchParams.endDate);
+  const [paymentTypes, setPaymentTypes] = useState(
+    new Set(searchParams.paymentTypes?.split(',').filter(val => val))
+  );
+
+  const formatDate = (date: string) => {
+    const parsedDate = parse(date, 'd.M.yyyy', new Date());
+    return formatISO(parsedDate, { representation: 'date' });
+  };
+
+  const handleSubmit = () =>
+    onSearch({
+      startDate: startDate ? formatDate(startDate) : '',
+      endDate: endDate ? formatDate(endDate) : '',
+      paymentTypes: joinSet(paymentTypes),
+      status,
+      q: query,
+    });
+
   return (
     <div className={styles.container}>
       <div className={styles.row}>
         <RefundStatusSelect
           className={styles.statusSelect}
           value={status}
-          onChange={value =>
-            setLocalSearchParams({ ...localSearchParams, status: value })
-          }
+          onChange={setStatus}
         />
         <div className={styles.dateRange}>
           <DateInput
             className={styles.startDate}
             id="startDate"
-            value={startDate && formatDateDisplay(startDate)}
-            onChange={(_, date) =>
-              setLocalSearchParams({
-                ...localSearchParams,
-                startDate: formatISO(date, { representation: 'date' }),
-              })
-            }
+            value={startDate}
+            onChange={setStartDate}
           />
-          <div className={styles.dateSeprator} />
+          <div className={styles.dateSeparator} />
           <DateInput
             className={styles.endDate}
-            id="sendDate"
-            value={endDate && formatDateDisplay(endDate)}
-            onChange={(_, date) =>
-              setLocalSearchParams({
-                ...localSearchParams,
-                endDate: formatISO(date, { representation: 'date' }),
-              })
-            }
+            id="endDate"
+            value={endDate}
+            onChange={setEndDate}
           />
         </div>
         <SearchInput
@@ -64,47 +71,34 @@ const RefundsSearch = ({
           label=""
           // HDS search input does not support passing a default value
           // so we have to use the placeholder property here
-          placeholder={q || t(`${T_PATH}.searchPlaceholder`)}
-          onSubmit={value => onSearch({ ...localSearchParams, q: value })}
+          placeholder={query || t(`${T_PATH}.searchPlaceholder`)}
+          onSubmit={() => handleSubmit()}
+          onChange={setQuery}
+          hideSearchButton
         />
       </div>
       <div className={styles.row}>
-        <SelectionGroup direction="horizontal">
-          <Checkbox
-            id="webshop"
-            name="paymentType"
-            label={t(`${T_PATH}.webshop`)}
-            checked={paymentTypeArray.includes(PaymentType.ONLINE_PAYMENT)}
-            onChange={e => {
-              const newPaymentTypes = e.target.checked
-                ? [...paymentTypeArray, PaymentType.ONLINE_PAYMENT]
-                : paymentTypeArray.filter(
-                    item => item !== PaymentType.ONLINE_PAYMENT
-                  );
-              setLocalSearchParams({
-                ...localSearchParams,
-                paymentTypes: newPaymentTypes.join(','),
-              });
-            }}
-          />
-          <Checkbox
-            id="cash"
-            name="paymentType"
-            label={t(`${T_PATH}.cash`)}
-            checked={paymentTypeArray.includes(PaymentType.CASHIER_PAYMENT)}
-            onChange={e => {
-              const newPaymentTypes = e.target.checked
-                ? [...paymentTypeArray, PaymentType.CASHIER_PAYMENT]
-                : paymentTypeArray.filter(
-                    item => item !== PaymentType.CASHIER_PAYMENT
-                  );
-              setLocalSearchParams({
-                ...localSearchParams,
-                paymentTypes: newPaymentTypes.join(','),
-              });
-            }}
-          />
-        </SelectionGroup>
+        <FilterOptions
+          label={t(`${T_PATH}.paymentType`)}
+          state={paymentTypes}
+          onChange={setPaymentTypes}
+          options={[
+            {
+              i18n: 'enums.paymentType.cashierPayment',
+              value: PaymentType.CASHIER_PAYMENT,
+            },
+            {
+              i18n: 'enums.paymentType.onlinePayment',
+              value: PaymentType.ONLINE_PAYMENT,
+            },
+          ]}
+        />
+        <Button
+          className={styles.searchButton}
+          iconLeft={<IconSearch />}
+          onClick={() => handleSubmit()}>
+          {t(`${T_PATH}.searchButton`)}
+        </Button>
       </div>
     </div>
   );
