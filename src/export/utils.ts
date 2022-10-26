49472b1d431be87995ff569774b187121c955604
Daniel Prange <prange.daniel@gmail.com>
2022-10-26 15:06:36 +0300

    Add formatGdprApiUrl, new env variable
    Also change useExportData to accept URL objects.

--- src/export/utils.ts
+++ src/export/utils.ts
@@ -9,6 +9,12 @@ export function formatExportUrl(
   return `${baseUrl}/${dataType}?${queryParams}`;
 }
 
+export function formatGdprApiUrl(path: string[]): URL {
+  const baseUrl = getEnv('REACT_APP_PARKING_PERMITS_GDPR_API_URL');
+  const joinedPath = Array.isArray(path) ? path.join('/') : path;
+  return new URL(joinedPath, baseUrl);
+}
+
 export function formatExportUrlPdf(dataType: string, objectId: string): string {
   const baseUrl = getEnv('REACT_APP_PARKING_PERMITS_EXPORT_URL_PDF');
   const queryParams = new URLSearchParams({
