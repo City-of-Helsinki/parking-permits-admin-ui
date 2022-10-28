67ce93d451c44d6067c0a9d235e1607d8441c2b1
Aman Yadav <amanpdyadav@gmail.com>
2022-10-28 09:45:06 +0300

    Navigate the user to permits page if not super admin
    Refs PV-280

--- src/components/SuperAdminLayout.tsx
+++ src/components/SuperAdminLayout.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import { Navigate } from 'react-router-dom';
+import useUserRole, { UserRole } from '../api/useUserRole';
 import Divider from './common/Divider';
 import Header from './Header';
 import SideNav from './superAdmin/SideNav';
 import styles from './SuperAdminLayout.module.scss';
 
-const SuperAdminLayout = (): React.ReactElement => (
-  <div className={styles.mainLayout}>
-    <Header />
-    <Divider />
-    <div className={styles.contentContainer}>
-      <div className={styles.superAdminSideNav}>
-        <SideNav />
-      </div>
-      <div className={styles.superAdminContent}>
-        <Outlet />
+const SuperAdminLayout = (): React.ReactElement => {
+  const userRole = useUserRole();
+  if (userRole < UserRole.SUPER_ADMIN) {
+    return <Navigate to="/permits" />;
+  }
+  return (
+    <div className={styles.mainLayout}>
+      <Header />
+      <Divider />
+      <div className={styles.contentContainer}>
+        <div className={styles.superAdminSideNav}>
+          <SideNav />
+        </div>
+        <div className={styles.superAdminContent}>
+          <Outlet />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SuperAdminLayout;
