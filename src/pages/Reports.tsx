5aabd6e82969feef504037e738e8cd00d2f2c379
Mika Hietanen <m.hietanen84@gmail.com>
2022-10-31 13:31:39 +0200

    Remove reports-section

--- src/pages/Reports.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { makePrivate } from '../auth/utils';
-
-const Reports = (): React.ReactElement => <div>This is reports page</div>;
-export default makePrivate(Reports);
