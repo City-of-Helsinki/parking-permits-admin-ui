2aebb5bd8d6c6d39097ef82f299e377aec7c4ee0
Aman Yadav <amanpdyadav@gmail.com>
2022-11-11 12:26:13 +0200

    Show login button if user is not logged
    Refs PV-280

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Outlet } from 'react-router-dom';
 import useUserRole, { UserRole } from '../api/useUserRole';
+import Login from '../pages/Login';
 import Divider from './common/Divider';
 import Header from './Header';
 import styles from './MainLayout.module.scss';
@@ -19,6 +20,7 @@ const MainLayout = (): React.ReactElement => {
           {userRole === UserRole.NON_AD_GROUPS && (
             <div>{t(`message.unauthorized`)}</div>
           )}
+          {userRole === UserRole.UNKNOWN && <Login />}
         </div>
       </div>
     </div>
