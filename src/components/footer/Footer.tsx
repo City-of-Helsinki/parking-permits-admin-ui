1c1412ed74059a7db31be0fba2c00d43977df3bd
Mika Hietanen <m.hietanen84@gmail.com>
2023-07-06 09:52:22 +0300

    Add footer-panel
    Refs #PV-604, #PV-531

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,39 @@
+import { Footer as HDSFooter } from 'hds-react';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+const T_PATH = 'components.footer';
+
+const Footer = (): React.ReactElement => {
+  const { t } = useTranslation();
+  const FOOTER_COLOR = 'var(--color-white)';
+  return (
+    <HDSFooter
+      theme={{
+        '--footer-background': 'var(--color-black)',
+        '--footer-color': FOOTER_COLOR,
+        '--footer-divider-color': FOOTER_COLOR,
+        '--footer-focus-outline-color': FOOTER_COLOR,
+      }}>
+      <HDSFooter.Base copyrightHolder={t(`${T_PATH}.copyright`)}>
+        <HDSFooter.Item
+          target="_blank"
+          href="https://www.hel.fi/fi/kaupunkiymparisto-ja-liikenne/pysakointi"
+          label={t(`${T_PATH}.parkingInfo`)}
+        />
+        <HDSFooter.Item
+          target="_blank"
+          href="https://www.hel.fi/static/liitteet-2019/Kaupunginkanslia/Rekisteriselosteet/Kymp/Maksullisen%20pys%C3%A4k%C3%B6innin%20ja%20pys%C3%A4k%C3%B6intitunnusten%20asiakasrekisteri.pdf"
+          label={t(`${T_PATH}.registryDescription`)}
+        />
+        <HDSFooter.Item
+          target="_blank"
+          href="https://www.hel.fi/fi/paatoksenteko-ja-hallinto/tietoa-helsingista/tietosuoja-ja-tiedonhallinta/tietosuoja"
+          label={t(`${T_PATH}.dataProtection`)}
+        />
+      </HDSFooter.Base>
+    </HDSFooter>
+  );
+};
+
+export default Footer;
