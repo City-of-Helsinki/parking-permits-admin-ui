b34ee902d2a6092cab49017c2757be372f96ca48
Toni Pellinen <toni.pellinen@anders.com>
2024-09-17 13:04:50 +0300

    Convert to use HDS login component
    This is a major change to all authentication
    and graphQl components.
    
    Refs: PV-876

--- src/api/ApiClientProvider.tsx
+++ src/api/ApiClientProvider.tsx
@@ -3,9 +3,8 @@ import {
   ApolloProvider,
   NormalizedCacheObject,
 } from '@apollo/client';
-import React, { FC, useContext } from 'react';
-import { ApiAccessTokenContext } from '../auth/ApiAccessTokenProvider';
-import { ApiAccessTokenActions } from '../auth/hooks';
+import { getApiTokensFromStorage } from 'hds-react';
+import React, { FC } from 'react';
 import { getEnv } from '../utils';
 import getApiClient from './client';
 
@@ -21,10 +20,7 @@ interface Props {
 }
 
 export const ApiClientProvider: FC<Props> = ({ children }) => {
-  const { getTokens } = useContext(
-    ApiAccessTokenContext
-  ) as ApiAccessTokenActions;
-  const tokens = getTokens();
+  const tokens = getApiTokensFromStorage();
   const apiToken = tokens
     ? tokens[getEnv('REACT_APP_PARKING_PERMITS_AUDIENCE')]
     : '';
