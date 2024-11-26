616e0b581d02bff737ce871f0004e22b9048788e
Toni Pellinen <toni.pellinen@anders.com>
2024-11-26 14:13:29 +0200

    Use useApiTokensClientTracking to refresh client tokens when tokens are renewed.

--- src/api/ApiClientProvider.tsx
+++ src/api/ApiClientProvider.tsx
@@ -3,7 +3,7 @@ import {
   ApolloProvider,
   NormalizedCacheObject,
 } from '@apollo/client';
-import { getApiTokensFromStorage } from 'hds-react';
+import { getApiTokensFromStorage, useApiTokensClientTracking } from 'hds-react';
 import React, { FC } from 'react';
 import { getEnv } from '../utils';
 import getApiClient from './client';
@@ -20,6 +20,7 @@ interface Props {
 }
 
 export const ApiClientProvider: FC<Props> = ({ children }) => {
+  useApiTokensClientTracking();
   const tokens = getApiTokensFromStorage();
   const apiToken = tokens
     ? tokens[getEnv('REACT_APP_PARKING_PERMITS_AUDIENCE')]
