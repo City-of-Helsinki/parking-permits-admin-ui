8467c6f1b60f291dc1c86fc96272f0854d180e8e
Toni Pellinen <toni.pellinen@anders.com>
2024-11-26 14:18:38 +0200

    Fix page contents emptying when tokens are renewed.
    Refs PV-881

--- src/auth/utils.tsx
+++ src/auth/utils.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import { Navigate } from 'react-router';
 import { useIsAuthorizationReady } from './useIsAuthReady';
-
-const T_PATH = 'components.common.utils';
 
 // eslint-disable-next-line import/prefer-default-export
 export function makePrivate<T>(
   Component: React.ComponentType<T>
 ): React.ComponentType<T> {
   return function PrivateComponent(props: T) {
-    const { t } = useTranslation();
-    const [isReady, loading, isAuthenticated] = useIsAuthorizationReady();
+    const [, , isAuthenticated] = useIsAuthorizationReady();
 
     if (!isAuthenticated) {
       return <Navigate to="/login" />;
     }
-    if (!isReady && loading) {
-      return <div>{t(`${T_PATH}.loading`)}</div>;
-    }
+
     return <Component {...props} />;
   };
 }
